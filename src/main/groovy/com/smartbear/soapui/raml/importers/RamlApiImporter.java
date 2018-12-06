db44189ce026ab5b46970334465dca442101ceed
Alexander Paeshin <Alexander.Paeshin@smartbear.com>
2018-12-06 10:07:56 +0300

    RIA-9524: Update ready-api-soapui-pro dependency

--- src/main/groovy/com/smartbear/soapui/raml/importers/RamlApiImporter.java
+++ src/main/groovy/com/smartbear/soapui/raml/importers/RamlApiImporter.java
@@ -7,11 +7,14 @@ import com.eviware.soapui.plugins.ApiImporter;
 import com.eviware.soapui.plugins.PluginApiImporter;
 import com.smartbear.soapui.raml.actions.ImportRamlAction;
 
+import javax.swing.JDialog;
 import java.util.ArrayList;
 import java.util.List;
 
-@PluginApiImporter( label = "RAML" )
+@PluginApiImporter(label = "RAML")
 public class RamlApiImporter implements ApiImporter {
+    private JDialog owner;
+
     @Override
     public List<Interface> importApis(Project project) {
 
@@ -19,13 +22,18 @@ public class RamlApiImporter implements ApiImporter {
         int cnt = project.getInterfaceCount();
 
         ImportRamlAction importRamlAction = new ImportRamlAction();
-        importRamlAction.perform( (WsdlProject)project, null );
+        importRamlAction.setOwner(owner);
+        importRamlAction.perform((WsdlProject) project, null);
 
-        for( int c = cnt; c < project.getInterfaceCount(); c++)
-        {
-            result.add( project.getInterfaceAt( c ));
+        for (int c = cnt; c < project.getInterfaceCount(); c++) {
+            result.add(project.getInterfaceAt(c));
         }
 
         return result;
+    }
+
+    @Override
+    public void setOwner(JDialog owner) {
+        this.owner = owner;
     }
 }
